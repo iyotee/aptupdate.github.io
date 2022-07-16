68a51ccc12ff318d6b243fce8cf320c66ea6c80f
iyotee <jeremy.noverraz@gmail.com>
2022-07-16 20:59:49 +0200

    Update omv.sh

--- omv.sh
+++ omv.sh
@@ -11,19 +11,21 @@ Second=`date +%S`
 echo `date`
 echo "Current Date is: $Day-$Month-$Year"
 echo "Current Time is: $Hour:$Minute:$Second"
-echo "=========== S T A R T I N G   S C R I P T =================="
+echo -e "${cyan}=========== S T A R T I N G   S C R I P T ==================${NC}"
 for (( counter=3; counter>0; counter-- ))
 do
+sleep 1
 echo -n "\n ."
 done
 printf "\n"
 echo "upgrading system, please wait until done"
 sudo curl https://iyotee.github.io/apt.sh | sudo bash
-echo "upgrading system finished successfully"
+echo -e "${green}UPGRADING SYSTEM SUCCESSFULLY${NC}"
 for (( counter=3; counter>0; counter-- ))
 do
+sleep 1
 echo -n "\n ."
 done
-echo "=========== S T A R T I N G       O  V  M      I N S T A L A T I O N     =================="
+echo -e "${cyan}=========== S T A R T I N G       O  V  M      I N S T A L A T I O N     ==================${NC}"
 sudo curl -sSL https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
-echo "O V M is successfully installed"
+echo -e "${green}O V M is successfully installed${NC}"
